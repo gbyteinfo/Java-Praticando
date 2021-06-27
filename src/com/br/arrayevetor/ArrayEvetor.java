f68998a1334c4c1c04899886d62e8bbfb2c201bf
NOTE-HST <jorge_mira@hst.com.br>
2021-06-27 20:16:00 -0300

    Array e Vetor - Percorrendo Array com For e inter~ção

--- src/com/br/arrayevetor/ArrayEvetor.java
+++ src/com/br/arrayevetor/ArrayEvetor.java
@@ -7,6 +7,7 @@ public class ArrayEvetor {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
+		int numDigitado;
 		String line = "\n";
 		
 		int[] num = new int[5];
@@ -23,11 +24,17 @@ public class ArrayEvetor {
 			System.out.printf("%s=> Numeros 1° array com for %d", line, num[i]);
 		}
 		
-		System.out.println(line);
-		
 		//PERCORRENDO O 2 ARRAY COM FOR
 		for(int i=0; i<3;i++) {
-			System.out.printf("%s=> Numeros 2° array com for %d", line, num2[i]);
+			System.out.printf("%s=> Numeros 2° array com for %d", line, num2[i],line);
+		}
+		
+		//PERCORRENDO O 2 ARRAY COM INTERAÇÃO DO USUARIO
+		System.out.printf("%sDigite 1, 2 ou 3: %s", line, line);
+		numDigitado = scan.nextInt();
+		
+		for(int i=0; i<numDigitado;i++) {
+			System.out.printf("%s => Digitado %d Numeros 2° array com for %d", line, numDigitado, num2[i]);
 		}
 	}
 	

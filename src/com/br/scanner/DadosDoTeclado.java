ee51eca2fdc7f61b8d9237061e65ff0abab6eaed
NOTE-HST <jorge_mira@hst.com.br>
2021-06-27 15:35:59 -0300

    Classe Scanner - Switch e ternaria incorporado

--- src/com/br/scanner/DadosDoTeclado.java
+++ src/com/br/scanner/DadosDoTeclado.java
@@ -10,21 +10,26 @@ public class DadosDoTeclado {
 		// E INSTANCIAÇÃO DO OBJETO scan DA CLASSE Scanner
 		
 		int numero1 = 0, numero2 = 0, resultado = 0;
-		String resposta;
+		String nome, resposta;
+		final int MEDIA = 60;//DECLARAÇÃO DE UMA CONSTANTE, IMUTAVEL
 		
-		System.out.println("Digite Primeiro numero");
+		//INTERAÇÃO
+		System.out.println("Digite seu nome:");
+		nome = scan.next();		
+		System.out.println("Digite Primeiro numero:");
 		numero1 = scan.nextInt();
-		System.out.println("Digite Segundo numero");
+		System.out.println("Digite Segundo numero:");
 		numero2 = scan.nextInt();
-		
 		resultado = numero1 + numero2;
 		
 		System.out.println("Quer ver o Resultado? Digite 'sim' ou 'nao'");
 		resposta = scan.next();
 		
+		//CONDIÇÃO SWITCH
 		switch (resposta) {
 		case "sim":
-			System.out.println("A soma dos Numeros é: " + resultado );
+			System.out.printf("Olá %s, primeiro numero %d, segundo numero %d, soma = %d ", nome, numero1, numero2, resultado);
+			System.out.printf("\n%s a Soma %d é "+(resultado < MEDIA ? "Menor" : "Maior")+" que %d", nome, resultado, MEDIA);//CONDIÇÃO TERNARIA INCORPORADA
 			break;
 		default:
 			System.out.println("Então blz, Tchau !!!");

d0f654b7febb6ddefb43b49e5169bced736e3ab5
NOTE-HST <jorge_mira@hst.com.br>
2021-06-27 14:06:46 -0300

    Condições Ternarias - Incorporando em parametros

--- src/com/br/condicoes/CondicoesTernaria.java
+++ src/com/br/condicoes/CondicoesTernaria.java
@@ -8,7 +8,9 @@ public class CondicoesTernaria {
 		int faltas = 4;
 		int maxFaltas = 5;
 		int media = 70;
+		int resultadoInt;
 		String resultado;
+		
 		
 		//CONDIÇÃO IF ELSE - SEM BREAK
 		if((nota >= media) && (faltas <= maxFaltas)) {
@@ -20,9 +22,16 @@ public class CondicoesTernaria {
 		}
 		System.out.println("if-else => Fim do programa");
 		
-		//CONDIÇÃO TERNARIA PADRÃO
+		//CONDIÇÃO TERNARIA - PADRÃO
 		resultado = (nota >= media ? "ternaria => Aprovado" : "ternaria => Reprovado");
-		System.out.println("\n"+resultado);
+		System.out.println("\n" + resultado);
+		
+		//CONDIÇÃO TERNARIA - INCORPORANDO EM PARAMETROS
+		
+		resultadoInt = (nota >= media ? 1 : 0);
+		System.out.println("\n"+(resultadoInt == 1 ? "ternaria2 => Aprovado" : "ternaria2 => Reprovado"));
+		
+		
 		
 		
 	}

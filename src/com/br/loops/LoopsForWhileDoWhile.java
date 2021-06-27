d06f9f57f61f9dc9329c1906f5f3bc39a6c41d0b
NOTE-HST <jorge_mira@hst.com.br>
2021-06-27 16:17:21 -0300

    Loops - For

---
+++ src/com/br/loops/LoopsForWhileDoWhile.java
@@ -0,0 +1,18 @@
+package com.br.loops;
+
+public class LoopsForWhileDoWhile {
+	
+	public static void main(String[] args) {
+		
+		
+		
+		//OPERAÇÕES
+		
+		//FOR INICIALIZAÇÃO DO CONTADOR DENTRO DO ESCOPO DO FOR
+		for(int cont = 0; cont < 10; cont ++ ) {
+			System.out.printf("\nJorge Mira contador em => %d", cont);
+		}
+		
+	}
+	
+}

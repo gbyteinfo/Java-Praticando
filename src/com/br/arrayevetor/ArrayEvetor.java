e35396951024c89bb25595957410fb5c319dda7d
NOTE-HST <jorge_mira@hst.com.br>
2021-06-27 19:06:49 -0300

    Array e Vetor - Array simples padrao

---
+++ src/com/br/arrayevetor/ArrayEvetor.java
@@ -0,0 +1,16 @@
+package com.br.arrayevetor;
+
+public class ArrayEvetor {
+
+	public static void main(String[] args) {
+		
+		
+		
+		int n1,n2,n3,n4;
+		int[] num = new int[5];
+		
+		System.out.printf("%d",num[4]);
+	}
+	
+}
+
--- src/com/br/loops/LoopsForWhileDoWhile.java
+++ src/com/br/loops/LoopsForWhileDoWhile.java
@@ -22,6 +22,7 @@ public class LoopsForWhileDoWhile {
 		
 		
 		
+		
 		//OPERAÇÕES
 		
 		//FOR INICIALIZAÇÃO DO CONTADOR DENTRO DO ESCOPO DO FOR OU FORA

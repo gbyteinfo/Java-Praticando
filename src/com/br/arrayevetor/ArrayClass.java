2ffaa5e2640547e47d83777abe18124afd51f6a6
NOTE-HST <jorge_mira@hst.com.br>
2021-06-27 23:53:53 -0300

    Array e Vetor - Verificando e comparando elementos do array

--- src/com/br/arrayevetor/ArrayClass.java
+++ src/com/br/arrayevetor/ArrayClass.java
@@ -7,18 +7,17 @@ public class ArrayClass {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int numDigitado = 3;
+		int tamanho = 4, numDigitado;
 		
 		String line = "\n";
-		int[] numeros = {10,20,30,40};
+		int[] numeros = {1,998,60,3};
 		int[] numeros2 = {1,9,5,26};
-		int[] numeros3 = new int[numDigitado];
+		int[] numeros3 = new int[tamanho];
 		int posicao;
 		
 		for(int i=0; i < 4; i++) {
 			System.out.printf("For percorrento | posição %d => %d%s", i, numeros[i], line);
 		}
-		
 		for(int n:numeros) {
 			System.out.printf("%sFor resumido | => %d", line, n);
 		}
@@ -30,13 +29,32 @@ public class ArrayClass {
 		}
 		
 		//MEDOTO FILL
-		System.out.printf("Digite um numero: %s", line);
+		System.out.printf("%sDigite um numero: %s", line, line);
 		numDigitado = scan.nextInt();
-		
 		Arrays.fill(numeros3, numDigitado);
 		for(int n:numeros3) {
 			System.out.printf("%sNumero digitado %d repete 3x", line, n);
 		}
+		System.out.print(line);
+		
+		//COPIANDO UM ARRAY PARA OUTRO
+		System.arraycopy(numeros2, 0, numeros3, 0, tamanho);
+		for(int n:numeros3) {
+			System.out.printf("%sNumeros copiados %d ", line, n);
+		}
+		
+		//COMPARAÇÃO DE ARRAYS IGUAIS OU NÃO
+		System.out.printf("%s%sArrays 2 e 3 são iguais? %s%s ",line, line, line, Arrays.equals(numeros2, numeros3) ? "SIM, SÃO IGUAI":"NÃO, NÃO SÃO IGUAIS");
+		
+		//VERIFICANDO ELEMENTOS
+		System.out.printf("%sDigite outro numero: %s",line, line);
+		int posicaoVerif=0, numeroEscolhido=0;
+		numeroEscolhido = scan.nextInt();
+		Arrays.sort(numeros);
+		posicaoVerif = Arrays.binarySearch(numeros, numeroEscolhido);
+		System.out.printf("%s%sO elemento %d esta no array? %s%s ",line, line, numeroEscolhido, line, posicaoVerif > 0 ? "SIM":"NÃO");
+		
+		
 	}
 	
 }

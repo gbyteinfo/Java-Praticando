2cfb2f1e7e2bd5c666d83ebddf89bb265144bb13
NOTE-HST <jorge_mira@hst.com.br>
2021-07-04 12:20:01 -0300

    Classes - Metodos e objetos static

--- src/com/br/classes/Jogador.java
+++ src/com/br/classes/Jogador.java
@@ -6,19 +6,21 @@ public class Jogador {
 	
 	String line = "\n";
 	private final int maxVidas = 3;
-	private int num = 99;
+	private int num = 0;
 	private int vidas = 0;
-	
 	static boolean alerta = false;
 	static int qtdJogadores = 0;
+	static int pontosJogadores = 0;
 	
 	public Jogador(int n) {
-		this.num = 10;
-		this.vidas = 6;
-		//INSTANCIAÇÃO DA CLASSE JOGADOR
-		System.out.printf("%sPosição => %d%s", line, n, line);
+		this.num = n;
+		this.vidas = 1;
+		
+		System.out.printf("%sJogador numero => %d criado!%s", line, n, line);
+		qtdJogadores++;
 	}
-
+	
+	
 	public int getVidas() {
 		return this.vidas;
 	}
@@ -26,6 +28,18 @@ public class Jogador {
 		if(this.vidas < maxVidas) {
 			this.vidas++;
 		}
+	}
+	
+	static void pontos() {
+		pontosJogadores+=10;
+	}
+	
+	public void info() {
+		System.out.printf("%sTotal Jogadores: %d",line, qtdJogadores);
+		System.out.printf("%sJogador Atual: %d",line, this.num );
+		System.out.printf("%sVidas: %d",line, this.vidas );
+		System.out.printf("%sAlerta: %s",line, (alerta ? "Sim" : "Não"));
+		System.out.printf("%sPontos do Jogador: %d", line, pontosJogadores);
 	}
 	
 	
--- src/com/br/classes/Principal.java
+++ src/com/br/classes/Principal.java
@@ -1,21 +1,30 @@
 package com.br.classes;
 
-import java.util.Scanner;
-
 public class Principal {
 	
 	//METODO MAIN, ELE SEMPRE IRA INDICAR QUE PROGRAMA STARTA AQUI
 	public static void main(String[] args) {
 		String line = "\n";
-		
 		int num = 0;
 		
-		System.out.printf("%sRetornou => %b%s",line, Jogador.alerta ? "Sim" : "Nao", line);
 		
 		Jogador j1 =  new Jogador(++num);//OBJETO J1 DO TIPO JOGADOR
 		Jogador j2 = new Jogador(++num);//OBJETO J2 DO TIPO JOGADOR
-		Jogador j3 = new Jogador(++num);//OBJETO J3 DO TIPO JOGADOR	
-		//FIM DA INSTANCIAÇÃO DA CLASSE JOGADOR
+		Jogador j3 = new Jogador(++num);//OBJETO J3 DO TIPO JOGADOR
+		Jogador j4 = new Jogador(++num);//OBJETO J3 DO TIPO JOGADOR
+		Jogador j5 = new Jogador(++num);//OBJETO J3 DO TIPO JOGADOR
+		
+		
+		j1.info();
+		System.out.printf(line);
+		j2.info();
+		System.out.printf(line);
+		j3.info();
+		System.out.printf(line);
+		j4.info();
+		System.out.printf(line);
+		j5.info();
+		
 		
 		
 	}

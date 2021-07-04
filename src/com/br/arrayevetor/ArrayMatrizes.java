5ae2d1eeb4eb374cda3e3f812b72d8cb7df904dc
NOTE-HST <jorge_mira@hst.com.br>
2021-07-04 00:02:34 -0300

    Ajustes

--- src/com/br/arrayevetor/ArrayMatrizes.java
+++ src/com/br/arrayevetor/ArrayMatrizes.java
@@ -1,5 +1,34 @@
 package com.br.arrayevetor;
+import java.security.SecureRandom;
+import java.util.Arrays;
 
 public class ArrayMatrizes {
-
+	public static void main(String[] args) {
+		
+		String line = "\n";
+		final int linhas = 3;
+		final int colunas = 5;
+		int[][] numeros = new int[linhas][colunas];
+		
+		for(int l=0 ; l<linhas ; l++) {
+			for(int c=0 ; c<colunas; c++) {
+				numeros[l][c] = new SecureRandom().nextInt(100);// PASSANDO O NUMERO MAX PARA ESCOLHA
+			}
+		}
+		
+		for(int l=0 ; l<linhas ; l++) {
+			for(int c=0 ; c<colunas; c++) {
+				System.out.printf("%2d|", numeros[l][c]);
+			}
+			System.out.printf("%n");
+		}
+		System.out.printf(line);
+		//COMPARAÇÃO DE ARRAYS IGUAIS OU NÃO
+		for(int[] n:numeros) {
+			for(int v:n) {
+				System.out.printf("%2d|", v);
+			}
+			System.out.printf("%n");
+		}
+	}
 }

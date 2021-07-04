273823acc0e98f0250c77d671ba3808104730fc7
NOTE-HST <jorge_mira@hst.com.br>
2021-07-04 01:26:32 -0300

    definições basicas de uma classe

--- src/com/br/classes/Jogador.java
+++ src/com/br/classes/Jogador.java
@@ -1,5 +1,18 @@
 package com.br.classes;
 
-public class Jogador {
+import java.util.Scanner;
 
+//MODIFICADOR DE ACESSO PUBLIC OU PRIVATE
+public class Jogador {
+	// scan QUE LE INFORMAÇÕES DO TECLADO COM A CLASSE Scanner
+	Scanner scan = new Scanner(System.in); //CRIAÇÃO DO OBJETO scan DA CLASSE Scanner
+	// E INSTANCIAÇÃO DO OBJETO scan DA CLASSE Scanner
+	
+	public Jogador(int n) {
+		
+		
+		//INSTANCIAÇÃO DA CLASSE JOGADOR
+		System.out.printf(" - posição => %d",n);
+	}
+	
 }
--- src/com/br/classes/Principal.java
+++ src/com/br/classes/Principal.java
@@ -1,10 +1,18 @@
 package com.br.classes;
 
+import java.util.Scanner;
+
 public class Principal {
 	
 	//METODO MAIN, ELE SEMPRE IRA INDICAR QUE PROGRAMA STARTA AQUI
 	public static void main(String[] args) {
 		
+		
+		
+		Jogador j1 =  new Jogador(1);//OBJETO J1 DO TIPO JOGADOR
+		Jogador j2 = new Jogador(2);//OBJETO J2 DO TIPO JOGADOR
+		Jogador j3 = new Jogador(3);//OBJETO J3 DO TIPO JOGADOR	
+		//FIM DA INSTANCIAÇÃO DA CLASSE JOGADOR
 	}
 	
 }

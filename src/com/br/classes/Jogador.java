8dd71747e841774edbd8a513a914abc0ac79ccd9
NOTE-HST <jorge_mira@hst.com.br>
2021-07-04 12:19:39 -0300

    Inclindo Jogadores

--- src/com/br/classes/Jogador.java
+++ src/com/br/classes/Jogador.java
@@ -5,13 +5,28 @@ package com.br.classes;
 public class Jogador {
 	
 	String line = "\n";
-	public int num =0;
+	private final int maxVidas = 3;
+	private int num = 99;
+	private int vidas = 0;
+	
+	static boolean alerta = false;
+	static int qtdJogadores = 0;
 	
 	public Jogador(int n) {
-		
 		this.num = 10;
-		
+		this.vidas = 6;
 		//INSTANCIAÇÃO DA CLASSE JOGADOR
 		System.out.printf("%sPosição => %d%s", line, n, line);
 	}
+
+	public int getVidas() {
+		return this.vidas;
+	}
+	public void addVidas() {
+		if(this.vidas < maxVidas) {
+			this.vidas++;
+		}
+	}
+	
+	
 }
--- src/com/br/classes/Principal.java
+++ src/com/br/classes/Principal.java
@@ -10,14 +10,14 @@ public class Principal {
 		
 		int num = 0;
 		
+		System.out.printf("%sRetornou => %b%s",line, Jogador.alerta ? "Sim" : "Nao", line);
+		
 		Jogador j1 =  new Jogador(++num);//OBJETO J1 DO TIPO JOGADOR
 		Jogador j2 = new Jogador(++num);//OBJETO J2 DO TIPO JOGADOR
 		Jogador j3 = new Jogador(++num);//OBJETO J3 DO TIPO JOGADOR	
 		//FIM DA INSTANCIAÇÃO DA CLASSE JOGADOR
 		
-		System.out.printf("%s%d",line, j1.num);
-		System.out.printf("%s%d",line, j2.num);
-		System.out.printf("%s%d",line, j3.num);
+		
 	}
 	
 }

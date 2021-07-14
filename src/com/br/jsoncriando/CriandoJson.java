2f68d3a1ac7e88cdda960e3e1cd2984f3585a053
Jorge-Mira <jorge@gbyteinfo.com.br>
2021-07-14 11:19:02 -0300

    Json - Criando

--- src/com/br/jsoncriando/CriandoJson.java
+++ src/com/br/jsoncriando/CriandoJson.java
@@ -1,22 +1,51 @@
 package com.br.jsoncriando;
 
+import java.io.FileWriter;
+import java.util.logging.Level;
+
+import com.br.org.JSONArray;
 import com.br.org.JSONObject;
+
+import com.sun.tools.javac.Main;
 
 public class CriandoJson {
 		
 	//JSON COMPOSTO DE OBJETOS, QUE POR SUA VEZ É UM CONJUNTO DE CHAVE E VALOR
 	public static void main(String[] args) {
-		//objetoJson = Sacar Dinheiro
+		String line = "\n";
+		
+		JSONObject objetoJson = new JSONObject(); //CRIANDO 1° OBJETO
+		objetoJson.put("nome", "guilherme");
+		objetoJson.put("senha", 252525);
+		objetoJson.put("status", true);
+		System.out.printf("Usuario 1 => %s%s", objetoJson.toString(), line);
+		
+		JSONObject objetoJson2 = new JSONObject(); //CRIANDO 2° OBJETO
+		objetoJson2.put("nome", "gbyteinfo");
+		objetoJson2.put("senha", 363636);
+		objetoJson2.put("status", false);
+		System.out.printf("Usuario 2 => %s%s", objetoJson2.toString(), line);
+		
+		JSONObject objetoJson3 = new JSONObject(); //CRIANDO 3° OBJETO
+		objetoJson3.put("nome", "gabriel");
+		objetoJson3.put("senha", 848484);
+		objetoJson3.put("status", true);
+		System.out.printf("Usuario 3 => %s%s", objetoJson3.toString(), line);
+		
+		JSONArray objetosJsonArray = new JSONArray(); //CRIANDO LISTA DE OBJETOS
+		objetosJsonArray.put(objetoJson); //ADICIONANDO OBJETO A LISTA
+		objetosJsonArray.put(objetoJson2); //ADICIONANDO OBJETO A LISTA
+		objetosJsonArray.put(objetoJson3); //ADICIONANDO OBJETO A LISTA
+		System.out.printf("Lista de objetos => %s", objetosJsonArray.toString());
+		
+		try(FileWriter arquivoJson = new FileWriter("usuarios.json")){ //LOCAL ARQUIVO .JSON
+			arquivoJson.write(objetosJsonArray.toString()); //LENDO E ADICIONANDO O ARRAY AO ARQUIVO
+			arquivoJson.flush();
+		}catch (Exception e) {
+			java.util.logging.Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, e) ;
+		}
 		
 		
-		JSONObject objetoJson = new JSONObject();
-		
-		objetoJson.put("Nome", "Jorge Mira");
-		objetoJson.put("Profissão", "Desenvolvedor");
-		
-		
-		System.out.printf("objeto => %s", objetoJson.get("Nome"));
-	
 	}
 	
 }
